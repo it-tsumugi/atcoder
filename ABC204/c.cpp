e1891ff7e72bb1b8f25d18ebf38bd1e88c13163b
it-tsumugi <tsumugi.it@gmail.com>
2021-06-08 10:55:59 +0900

    出力の修正

--- ABC204/c.cpp
+++ ABC204/c.cpp
@@ -2,16 +2,32 @@
 using namespace std;
 #define rep(i,n) for(int i=0;i<n;++i)
 typedef long long ll;
+const int MAX_N=2000;
 
-int main(void){
-    int N,M;
-    cin >> N >> M;
-    int A[N],B[M];
-    rep(i,M){
-        cin >> A[i];
-        cin >> B[i];
-    }
+vector<vector<int>>G;
+bool temp[MAX_N];
 
-    
-    return 0;
+void dfs(int v){
+	if(temp[v])return;
+	temp[v]=true;
+	for(auto vv:G[v])dfs(vv);
+}
+
+int main(){
+	int N,M;
+	cin >> N >> M;
+	G.resize(N);
+	for(int i=0;i<M;i++){
+		int a,b;
+		cin >> a >> b;
+		G[a-1].push_back(b-1);
+	}
+	int ans=0;
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++)temp[j]=false;
+		dfs(i);
+		for(int j=0;j<N;j++)if(temp[j])ans++;
+	}
+	
+	cout << ans <<endl;
 }
