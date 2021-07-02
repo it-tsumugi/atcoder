ee32924747c2437449c2925897d9b2d779357dbd
it-tsumugi <tsumugi.it@gmail.com>
2021-07-02 16:46:35 +0900

    ABC208

---
+++ ABC208/a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+typedef long long ll;
+
+int main(){
+    ll a,b,c;
+	vector <ll> vec(3);
+	cin >> a >> b >> c;
+
+	return 0;	
+}
